cf9cc1c382bc5b866c4aec02464d9101ae2d35db
Sukhseerat-Kaur <54837213+Sukhseerat-Kaur@users.noreply.github.com>
2021-07-07 01:47:41 +0530

    Delete Day16 directory

--- Day16/Card/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Card from "./components/card";
-import "./style.css";
-
-ReactDOM.render(<Card />, document.getElementById("body"));
--- Day16/chess-board/src/components/box.jsx
+++
@@ -1,6 +0,0 @@
-const n = 8;
-function boxes() {
-  return [...Array(n)].map((element, index) => <div className="box"></div>);
-}
-
-export default boxes;
